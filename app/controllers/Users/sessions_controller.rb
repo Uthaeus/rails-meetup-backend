40b67dcfe41b42f24d960ebd509ca9f9da590130
Uthaeus <romanlavery@gmail.com>
2023-05-03 15:57:12 -0600

    created rack session fix, included in registration and session controllers, created current_user controller

--- app/controllers/Users/sessions_controller.rb
+++ app/controllers/Users/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Users::SessionsController < Devise::SessionsController
   #before_action :configure_sign_in_params, only: [:create]
+  include RackSessionFix
   respond_to :json
 
   # GET /resource/sign_in
