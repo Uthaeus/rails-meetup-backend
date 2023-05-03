1ebcd37b60b28933baf5f45181a0bcfa6b6e5e77
Uthaeus <romanlavery@gmail.com>
2023-05-03 17:00:07 -0600

    added devise sign up parameters in application controller

--- app/controllers/Users/registrations_controller.rb
+++ app/controllers/Users/registrations_controller.rb
@@ -59,18 +59,20 @@ class Users::RegistrationsController < Devise::RegistrationsController
       render json: {
         status: {code: 200, message: 'Signed up sucessfully.'},
         data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
-      }
+      }, status: :ok
     elsif request.method == "DELETE"
       render json: {
         status: {code: 200, message: 'Account deleted sucessfully.'}
-      }
+      }, status: :ok
     else
       render json: {
-        status: {code: 422, message: 'User could not be created.'},
-        errors: resource.errors.full_messages
+        status: {code: 422, message: "User couldn't be created successfully. #{resource.errors.full_messages.to_sentence}"}
       }, status: :unprocessable_entity
     end
   end
+
+  
+
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
