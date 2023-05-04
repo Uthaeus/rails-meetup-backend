1d39c8ce9a38adb08eae955a55d8a47755b6e607
Uthaeus <romanlavery@gmail.com>
2023-05-03 20:03:20 -0600

    configuring cookies and session store

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,12 @@
 class ApplicationController < ActionController::API
-    #include ActionController::Cookies
+    # include ActionController::Cookies
     before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:username, :password])
   end
 
 end
--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,10 @@ module RailsMeetupBackend
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
-    # config.middleware.use ActionDispatch::Cookies 
+    config.session_store :cookie_store, key: ‘_interslice_session’
+
+    config.middleware.use ActionDispatch::Cookies 
+    config.middleware.use config.session_store, config.session_options
     # config.middleware.use ActionDispatch::Session::CookieStore
   end
 end
